92102c7676f33f37466952b64e25026350c9ee8b
SkateZen <mariusmdm123@gmail.com>
2023-02-20 02:09:17 +0100

    javascript : portfolio-filters

--- portfolio-2/js/global.js
+++ portfolio-2/js/global.js
@@ -1,8 +1,73 @@
-// commentaire
 
-console.log('toto');
 
-// string
+//Menu mobile
 
-let myVar = "ma variable";
-const myConst = 48;
+function menuMobile(){
+    const btn = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+
+    const links = document.querySelectorAll('.navbar a');
+
+    btn.addEventListener('click', () =>{
+        console.log('click');
+        header.classList.toggle('show-nav');
+    })
+
+    links.forEach(link =>{
+        link.addEventListener('click', () => {
+            header.classList.remove('show-nav');
+        });
+    })
+}
+
+menuMobile();
+
+//Portfolio
+
+function tabsFilter() {
+
+    const tabs = document.querySelectorAll('.portfolio-filters a');
+    const projets = document.querySelectorAll('.portfolio .card');
+
+    const resetActiveLinks = () => {
+
+        tabs.forEach(elem => {
+            elem.classList.remove('active');
+        });
+        
+    }
+
+    const showProjects = (elem) => {
+        console.log(elem);
+        projets.forEach(projet => {
+            
+            let filter = projet.getAttribute('data-category');
+
+            if(elem === 'all'){
+                projet.parentNode.classList.remove('hide');
+                return;
+            }
+
+            if (filter !== elem){
+                projet.parentNode.classList.add('hide');
+            } else{
+                projet.parentNode.classList.remove('hide');
+            }
+        });
+    }
+
+    tabs.forEach(elem => {
+        elem.addEventListener('click', (event) => {
+            event.preventDefault();
+            let filter = elem.getAttribute('data-filter');
+            console.log(filter);
+            showProjects(filter);
+            resetActiveLinks();
+            elem.classList.add('active');
+        });
+    })
+
+    // showProjects();
+}
+
+tabsFilter();

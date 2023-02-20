f94e5311b6ce168dd87f7ac0dc03c3b035a94c4f
SkateZen <mariusmdm123@gmail.com>
2023-02-20 18:48:41 +0100

    js : modal projet

--- portfolio-2/js/global.js
+++ portfolio-2/js/global.js
@@ -71,3 +71,31 @@ function tabsFilter() {
 }
 
 tabsFilter();
+
+function showProjectsDetails(){
+
+    const links = document.querySelectorAll('.card__link');
+    const modals = document.querySelectorAll('.modal');
+    const btns = document.querySelectorAll('.modal__close');
+
+    const hideModals = () => {
+        modals.forEach(modal => {
+            modal.classList.remove('show');
+        });
+    }
+
+    links.forEach(elem => {
+        elem.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.querySelector(`[id=${elem.dataset.id}]`).classList.add("show");
+        });
+    });
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            hideModals();
+        });
+    });
+}
+
+showProjectsDetails();
